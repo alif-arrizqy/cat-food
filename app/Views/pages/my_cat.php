105d9163c708428fc79913fb68a330c09a8a3bc2
alif-arrizqy <alif.arrizqy20@gmail.com>
2021-10-19 14:47:41 +0700

    update: modal add and modal edit

--- app/Views/pages/my_cat.php
+++ app/Views/pages/my_cat.php
@@ -41,55 +41,6 @@
                                 </li>
                             </ul> -->
                             <a href="#" class="btn btn-outline-success font-w-600 my-auto text-nowrap ml-auto add-event" data-toggle="modal" data-target="#addevent"><i class="icon-plus"></i> Tambah Kucing</a>
-                            <?php foreach ($data_cat as $dc) { ?>
-                                <!-- Modal -->
-                                <div id="addevent" class="modal fade" role="dialog">
-                                    <div class="modal-dialog text-left">
-                                        <!-- Modal content-->
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h4 class="modal-title" id="model-header">Tambah Kucing</h4>
-                                            </div>
-                                            <div class="modal-body">
-                                                <?php echo form_open('/main/add_cat/' . $dc['id_user']) ?>
-                                                <div class="row">
-                                                    <div class="col-md-12">
-                                                        <div class="form-group">
-                                                            <label for="start-date" class="">Nama Kucing</label>
-                                                            <div class="d-flex event-title">
-                                                                <input id="cat_name" type="text" placeholder="Isi nama kucing kamu" class="form-control" name="cat_name">
-                                                                <input id="id_user" type="hidden" class="form-control" name="id_user" value="<?= $dc['id_user'] ?>">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="col-md-6 col-sm-6 col-12">
-                                                        <div class="form-group start-date">
-                                                            <label for="start-date" class="">Usia Kucing</label>
-                                                            <div class="d-flex">
-                                                                <input id="age_cat" placeholder="Isi usia kucing kamu" class="form-control" type="text" name="age">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-6 col-sm-6 col-12">
-                                                        <div class="form-group end-date">
-                                                            <label for="end-date" class="">Berat Kucing</label>
-                                                            <div class="d-flex">
-                                                                <input id="weight_cat" placeholder="Isi berat kucing kamu" class="form-control" type="text" name="weight">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button id="discard" class="btn btn-outline-primary" data-dismiss="modal">Batal</button>
-                                                <button type="submit" id="add-event" class="btn btn-primary eventbutton">Simpan</button>
-                                            </div>
-                                            <?php echo form_close() ?>
-                                        </div>
-                                    </div>
-                                </div>
-                            <?php } ?>
                         </div>
                     </div>
                 </div>
@@ -149,9 +100,60 @@
 </main>
 
 <!-- Modal -->
+<?php foreach ($data_cat as $dc) { ?>
+    <!-- Modal Add Cat -->
+    <div id="addevent" class="modal fade" role="dialog">
+        <div class="modal-dialog text-left">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="model-header">Tambah Kucing</h4>
+                </div>
+                <div class="modal-body">
+                    <?php echo form_open('/main/add_cat/' . $dc['id_user']) ?>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="start-date" class="">Nama Kucing</label>
+                                <div class="d-flex event-title">
+                                    <input id="cat_name" type="text" placeholder="Isi nama kucing kamu" class="form-control" name="cat_name">
+                                    <input id="id_user" type="hidden" class="form-control" name="id_user" value="<?= $dc['id_user'] ?>">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-sm-6 col-12">
+                            <div class="form-group start-date">
+                                <label for="start-date" class="">Usia Kucing</label>
+                                <div class="d-flex">
+                                    <input id="age_cat" placeholder="Isi usia kucing kamu" class="form-control" type="text" name="age">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-12">
+                            <div class="form-group end-date">
+                                <label for="end-date" class="">Berat Kucing</label>
+                                <div class="d-flex">
+                                    <input id="weight_cat" placeholder="Isi berat kucing kamu" class="form-control" type="text" name="weight">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button id="discard" class="btn btn-outline-primary" data-dismiss="modal">Batal</button>
+                    <button type="submit" id="add-event" class="btn btn-primary eventbutton">Simpan</button>
+                </div>
+                <?php echo form_close() ?>
+            </div>
+        </div>
+    </div>
+    <!-- End of Modal Add Cat -->
+<?php } ?>
 <?php
 foreach ($data_cat as $dc) {
 ?>
+    <!-- Modal Edit Cat -->
     <div class="modal fade" id="exampleModal<?= $dc['id_cat'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -188,5 +190,6 @@ foreach ($data_cat as $dc) {
             </div>
         </div>
     </div>
+    <!-- End of Modal Edit Cat -->
 <?php } ?>
 <?= $this->endSection(); ?>
